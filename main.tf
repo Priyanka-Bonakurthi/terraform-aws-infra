c8d8d1f5dd54971e190f933fdb22cf1747ea101f
Priyanka-Bonakurthi <145551528+Priyanka-Bonakurthi@users.noreply.github.com>
2025-08-25 17:46:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "my_bucket" {
 
 # Create an EC2 instance
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 (us-east-1)
+  ami           = "ami-0360c520857e3138f" # Amazon Linux 2 (us-east-1)
   instance_type = var.instance_type
 
   tags = {
