5b37d546360bc047db98ece946705662c25c6e64
priyanka <bonakurthi.priyanka@kyndryl.com>
2025-08-25 16:56:24 +0530

    terraform files

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
+# Create an S3 bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-terraform-demo-bucket-12345"
+  acl    = "private"
+}
+
+# Create an EC2 instance
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 (us-east-1)
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
+
 
