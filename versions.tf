44e25645fdb2e77fa60290c3274842d8cb86f4e6
Priyanka-Bonakurthi <145551528+Priyanka-Bonakurthi@users.noreply.github.com>
2025-08-26 17:13:47 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,13 +1,17 @@
-terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.5"
-    }
-  }
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
 }
