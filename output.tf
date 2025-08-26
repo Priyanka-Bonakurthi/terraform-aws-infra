e3859cea0619200285f55ae935445addd46ceb78
Priyanka-Bonakurthi <145551528+Priyanka-Bonakurthi@users.noreply.github.com>
2025-08-26 17:45:18 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,15 @@
-
-output "ec2_public_ip" {
-  value = aws_instance.my_ec2.public_ip
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
 
-output "s3_bucket_name" {
-  value = aws_s3_bucket.priya_my_bucket.bucket
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "instance_id" {
+  value = aws_instance.web.id
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.demo.bucket
 }
